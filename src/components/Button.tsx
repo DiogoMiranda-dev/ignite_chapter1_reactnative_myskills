8cea86316ffecf8aeb23b351fc0c107385472e2c
Diogo Miranda <diogosminfor@gmail.com>
2022-01-28 14:26:55 -0300

    myskill typescript

---
+++ src/components/Button.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableOpacityProps,
+} from "react-native";
+
+interface ButtonProps extends TouchableOpacityProps {
+  title: string
+};
+
+export function Button({ title, ...rest }: ButtonProps) {
+  return (<TouchableOpacity
+    activeOpacity={0.7}
+    style={styles.button}
+    {...rest}
+  >
+    <Text style={styles.buttonText} >{title}</Text>
+  </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#A370F7',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
